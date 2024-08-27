c721d13a6af79295860681cf254c2ca665850ba6
Akiha-L <renjuin123@gmail.com>
2024-08-27 10:50:14 +0800

    Add files via upload

--- pages/10_🪐_space titanic.py
+++ pages/10_🪐_space titanic.py
@@ -5,6 +5,15 @@ import joblib
 import pandas as pd
 import numpy as np
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 # 顯示網頁畫面
 
